6735ef82008fab33b93a5472e63d9d816a5008a9
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2019-12-03 15:32:30 +0900

    アカウント用のURLを設定

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('task/', include('taskmanager.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
